1b45eae93a6c4f921b040133458ea50dc38b1cb4
Kathie Huang <kathiehuang@hotmail.com>
2023-12-29 14:17:40 -0800

    Added analytics tracking on vercel

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+import { Analytics } from '@vercel/analytics/react';
+ 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
+
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
